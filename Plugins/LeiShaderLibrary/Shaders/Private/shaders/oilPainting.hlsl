972c38694f07fd811c699f9bd9a4fd1dfb954b15
leiwu05 <leiwu05@outlook.com>
2025-05-11 22:15:01 -0400

    Commit - Fix brush size adapting issue

--- Plugins/LeiShaderLibrary/Shaders/Private/shaders/oilPainting.hlsl
+++ Plugins/LeiShaderLibrary/Shaders/Private/shaders/oilPainting.hlsl
@@ -116,23 +116,23 @@ float4 Paint(Texture2D tex, float2 uv)
     float aspect = RES.x / RES.y;
 
     // Canvas Texture
-    float canv = 10.0;
+    float canv = 0.0;
 
-//    canv = max(canv, (noise(uv * float2(0.7, 0.03).xy)).x);
-//    canv = max(canv, (noise(uv * float2(0.7, 0.03).yx * aspect)).x);
-//    canv = pow(canv, 3); //return canv;
-//    canv = float4(float(0.93 + 0.07 * canv).xxx, 1.0);
+    canv = max(canv, (noise(uv * float2(0.7, 0.03).xy)).x);
+    canv = max(canv, (noise(uv * float2(0.7, 0.03).yx * aspect)).x);
+    canv = pow(canv, 3); //return canv;
+    canv = float4(float(0.93 + 0.07 * canv).xxx, 1.0);
 
     // Brush Layers
     float3 brushPos;
 
-    int NumGrid = int(float(0x10000 / 2.5) * min(pow(RES.x / 1920.0, 5.23), 1.0) * (1.0 - layerScale * layerScale));
-    //NumGrid = 255;
+    int NumGrid = int(float(0x10000 / 1) * min(pow(RES.x * RES.y / 1920. / 1440., .5), 1.0) * (1.0 - layerScale * layerScale));
+    //NumGrid = 10000;
     
     int NumX = int(sqrt(float(NumGrid) * aspect) + 0.5);
     int NumY = int(sqrt(float(NumGrid) / aspect) + 0.5);
-    float gridW0 = /*RES.x*/ 1 / float(NumX);
-    
+    float gridW0 = /*RES.x*/ 1. / float(NumX);
+//    return float4(float(NumGrid)/10000.,1,1,1);
     int gridsum = 0;
     // min-scale layer has at least 10 strokes in y
     // NumY*layerScaleFact^maxLayer==10
@@ -147,11 +147,11 @@ float4 Paint(Texture2D tex, float2 uv)
 
         int NumX2 = int(float(NumX) * pow(layerScale, float(layer)) + 0.5);
         int NumY2 = int(float(NumY) * pow(layerScale, float(layer)) + 0.5);
-        float gridW = /*RES.x*/ 1 / float(NumX2);
+        float gridW = /*RES.x*/ 1. / float(NumX2);
 
         
         int n0 = int(dot(floor(float2(uv/* / RES.xy*/ * float2(NumX2, NumY2))), float2(1, NumX2)));
-		//return float(n0) / NumGrid;
+//		return float(n0) / NumGrid;
 		
         for (int ni = 0; ni < 25; ni++) //nine near grids
         {
@@ -164,30 +164,30 @@ float4 Paint(Texture2D tex, float2 uv)
             brushPos.x += gridW * 0.5 * (float((gridn / NumX2) % 2) - 0.5);
 
             //randomize brushPos
-            brushPos.xy += gridW * (noise(gridsumn + layer * 123).xy - 0.5)*0.8;
+            brushPos.xy += gridW * (noise(gridsumn + layer * 123).xy - 0.5);//*0.8;
 
             //Brush Direction -----------
             float2 grad = getGrad(tex, brushPos.xy, gridW * 1.)+getGrad(tex, brushPos.xy,gridW*.1);
             
             grad /= 2.;
-            grad += sin(uv*100.) * 5.45; // add random noise (for plain area
+            grad += sin(uv*100.) * .005; // add random noise (for plain area
             
             float l = length(grad);
             float2 n = normalize(grad);
             float2 t = N(n);
-            //return float4(grad,1.,1.);
             
             
             // width and length of brush stroke --------------------------------------------------------------------------------------------------------------
-            float bw = (gridW-.6*gridW0)*4.8; //gridW * .75;
+            float bw = (gridW-.6*gridW0)*1.2; //gridW * .75;
+            
             float bl = bw;
-    		float stretch=sqrt(1.43*pow(1.,1./float(layer+1)));
-		    bw*=BrushSize*(.4+.4*noise(gridsumn).y)/stretch;
-		    bl*=BrushSize*(.4+.4*noise(gridsumn).z)*stretch;
+    		float stretch=sqrt(1.43*pow(3.,1./float(layer+1)));
+		    bw*=BrushSize*(.8+.4*noise(gridsumn).y)/stretch;
+		    bl*=BrushSize*(.8+.4*noise(gridsumn).z)*stretch;
 		    
  		    bw /= 1.-.25*abs(StrokeBend);
-		    bw = (l*.008 < bw && bw < gridW0*10 && layer != maxLayer) ? 0 : bw; 
-            
+		    bw = (l*.001 < bw && bw < gridW0*0.8 && layer != maxLayer) ? 0 : bw; 
+		    
             // convert to local space --------------------------------------------------------------------------------------------------------------
         	float2 uv_screen = (uv - brushPos.xy) * float2(aspect, 1.);
             float2 pos = float2(dot(uv_screen, n), dot(uv_screen, t)) / float2(bw, bl);
@@ -202,34 +202,27 @@ float4 Paint(Texture2D tex, float2 uv)
 
 
 			// Brush shape ----------------------------------------------------------------------------------------------------------------------
-			float s = 0;
-			if (pos.x>=0 && pos.x<=1 && pos.y>=0 && pos.y<=1)
-			{
-				s = 1.;
+			float s = 0; float mask;
+			
 			    s = pos.x * (1. - pos.x) * 4. * pos.y * (1. - pos.y) * 4. * 2.53;
 				s = clamp((s - 0.5) * 2.,0.,1.);
 				// ----------------------------------------------
-				float fur = noise(pos * float2(0.01,0.001) * 4.4).x + noise(pos * float2(0.01,0.001) * 0.31).x;
-				
 				float s_ = s;
-				float2 pos_ = pos;
-				
+				float fur = noise(pos * float2(0.01,0.001) * 4.4).x + noise(pos * float2(0.01,0.001) * 0.31).x;
 				s *= fur;
 				// ----------------------------------------------
-				float mask = 1 - pos_.y;
-				mask *= max(
-					pow( (cos(pos_.x * 3.14159 * 2 + 1.5 * (noise(gridsumn).x-.5))+1.)/2., 1.2) - 0.135, 
-					0.35 * exp(-pos_.y * pos_.y * 5) * (1. - pos_.y)
-				);
-				mask = clamp(mask+0.6, 0., 1.);
-			
-				s = s * 0.43 + s_ * mask * 0.55;
-				s = pow(s, 1.2)  * pow(1.285 - pos_.y, 0.8) - .25 * pos_.y;
-				s = clamp(s,0.,1.);
-			}
-            
+				mask = max(
+					pow( (cos(pos.x * 3.14159 * 2. + 1.5 * (noise(-gridsumn).x-.5))+1.)/2., 1.2) - 0.135, 
+					0.35 * exp(-pos.y * pos.y * 5.) * (1. - pos.y)
+				) * (1. - pos.y);
+				mask = clamp(mask+0.3, 0., 1.);
+				s = s * 0.43 + s_ * mask * .55 - .5 * pos.y;
+				
+				s = clamp(s,0.,1.) * step(-0.5,-abs(pos.x-.5)) * step(-0.5,-abs(pos.y-.5));
+				//s = pow(s, 1.2) * pow(1.285 - pos.y, 0.8) - .25 * pos.y;
+
             float4 dcoll;
-            dcoll.xyz = getColor(tex, brushPos.xy, 1.0);
+            dcoll.xyz = getColor(tex, brushPos.xy, 1.0);// * lerp(s*.13+.87,1.,mask);
             col = lerp(col, dcoll, s);
 //            dcoll = debugCircle(uv, brushPos.xy, gridW / 4, aspect, float(layer) / float(maxLayer));
 //            col = lerp(col, dcoll, dcoll.w);
@@ -246,11 +239,11 @@ float4 main (float4 fragCoord : SV_POSITION): SV_TARGET
     Functions F;
     F.BrushDetail = 0.1;
     F.StrokeBend = -1.;
-    F.BrushSize = 1.;
+    F.BrushSize = 1.5;
     F.layerScale = float(QUALITY_PERCENT) / 100.0;
     F.SrcContrast = 1.4;
-    F.SrcBright = 1.;
-    F.RES = float2(1920, 1440);
+    F.SrcBright = 0.9;
+    F.RES = float2(1920, 1080)*1.25;
 
     return F.Paint(srctex, uv);
 }
