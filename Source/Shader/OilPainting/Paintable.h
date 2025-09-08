fe916ddb9135677d375c11b4d2b5c01d540b8f5e
Lei <leiwu05@outlook.com>
2025-09-08 09:21:47 -0400

    feat: two cpp interfaces

---
+++ Source/Shader/OilPainting/Paintable.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Paintable.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI, Blueprintable)
+class UPaintable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class SHADER_API IPaintable
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Painting")
+	void OnRayHit(const FVector& HitPosition);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Painting")
+	void OnRayExit();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Painting")
+	void SetBrushSize(float SizeOffset);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Painting")
+	void SetBrushHardness(float HardnessOffset);
+};
---
+++ Source/Shader/OilPainting/Painter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Painter.h"
+
+// Add default functionality here for any IPainter functions that are not pure virtual.
